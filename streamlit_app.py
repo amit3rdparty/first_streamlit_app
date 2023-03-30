68f22808f70cfe99ea1d6b79e2a5f216c17923b9
amit3rdparty <129420496+amit3rdparty@users.noreply.github.com>
2023-03-31 00:15:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('My Parent healthy Diner')
+/*streamlit.title('My Parent healthy Diner')*/
+streamlit.title('Breakfast Menu')
+streamlit.title('Dhokla')
+streamlit.title('Aalu Parantha')
+streamlit.title('Idli Dosa')
